3ee9969625c08bd3b4cc40d2ec47be71faaafbe8
mireie <michael.reiersgaard@gmail.com>
2021-06-04 16:55:01 -0700

    cleanup code formatting html, css, js

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
     
-    
     const q1 = $("#q1").val();
     const q2 = parseInt($("input:radio[name=q2]:checked").val());
     const q3 = parseInt($("input:radio[name=q3]:checked").val());
@@ -23,46 +22,30 @@ $(document).ready(function() {
     const color = $("#color").val();
     const score = totalScore(q2,q3,q4,q5,q6);
     
-
     if (score % 7 === 0) {
       result= "Go";
     } else {
       if (q2 === 0) {
-        //Animal
         if (score % 2 === 0) {
-          //Swift
           result= "Swift";
         } else {
-          //Python
           result= "Python";
         }
       } else if (q2 === 1) {
-        //Food
         if (score % 2 === 0) {
-          //C#
           result= "C#";
         } else {
-          //JavaScript
           result= "JavaScript";
         }
       } else if (q2 === 2) {
-        //Red
         if (score % 2 === 0) {
-          //Ruby
           result= "Ruby";
         } else {
-          //Rust
           result= "Rust";
         }
       }
     }
     $("#result").text(result);
-
-    //debugging
-    const debug = [q1,q2,q3,q4,q5,q6,color].join(' | ');
-    console.log(debug);
-    console.log("Total Score: "+score);
-    
     $("body").css("background-color",color);
     $("#name").text(q1);
     $(".result").slideToggle(1000);
@@ -78,6 +61,11 @@ $(document).ready(function() {
     $('#validation').removeClass('submitted');
     $('#validation').hide();
     $("body").css("background-color","white");
-    
+
+  /* debugging tools
+    const debug = [q1,q2,q3,q4,q5,q6,color].join(' | ');
+    console.log(debug);
+    console.log("Total Score: "+score);
+    */  
   });
 })
